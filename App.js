5f81baadfa7cbcb5507ee819d7056a50113a5271
BFSmx5 <97541881+BSFmx5@users.noreply.github.com>
2022-03-22 15:09:28 -0600

    Traducción 1.0

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import BottomTabNavigator from "./components/BottomTabNavigator";
 
 export default class App extends Component {
   render() {
-    //add the return satement for BottomTabNavigator- challenge 4
+    //Añadir la declaración de retorno paraBottomTabNavigator
+    return <BottomTabNavigator />;
   }
 }
--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -1,20 +1,17 @@
 import React, { Component } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-//import createBottomTabNavigator libray -Challenge 1
+//Importa las librerias
+
 
 import TransactionScreen from "../screens/Transaction";
 import SearchScreen from "../screens/Search";
+// Crea tab
 
-//Add the createBottomTabNavigator to the variable called tab-Challenge 2
 export default class BottomTabNavigator extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <Tab.Navigator>
-      //Add the code to move to the respective screen when tab is clicked-challenge 3
-         
-        </Tab.Navigator>
-      </NavigationContainer>
+      //Añade el código para añadir navigationcontainer
+
+      
     );
   }
 }
